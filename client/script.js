8c04e0e67a49ab458b5b842003226ac5c0aeb4d1
Suryansh7531 <suryansh8248@gmail.com>
2023-03-03 21:34:47 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -101,7 +101,8 @@ const handleSubmit = async (e) => {
     //fetch data from server->bot's response
     // const response = await fetch('https://codex-im0y.onrender.com/', {
     // const response = await fetch('http://localhost:5000', {
-    const response = await fetch('http://localhost:5000', {
+    // const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codex-chatbot-drlg.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
